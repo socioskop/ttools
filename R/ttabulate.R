553b3e473dd254fc72f6107df5596495866245de
socioskop <anders@socioskop.dk>
2021-09-09 21:36:36 +0200

    adding na.count option

--- R/ttabulate.R
+++ R/ttabulate.R
@@ -24,6 +24,8 @@ ttabulate <- function(data, xs, treat, num=NA, cat=NA, bin=NA, dichotomize=NA, c
   t <- data.frame()
   for (x in xs){
     print(paste0("working on ", x))
+    
+    # generate segment
     if (x %in% "n"){try(t <- dplyr::bind_rows(t, twoway_n  (data=data, x, treat)))}
     if (x %in% num){try(t <- dplyr::bind_rows(t, twoway_num(data=data, x, treat, digit.m = 2, digit.sd = 2)))}
     if (x %in% cal.date){try(t <- dplyr::bind_rows(t, twoway_num(data=data, x, treat, digit.m = 2, digit.sd = 2, cal.date==T)))}
@@ -31,7 +33,24 @@ ttabulate <- function(data, xs, treat, num=NA, cat=NA, bin=NA, dichotomize=NA, c
     if (x %in% dichotomize & !x %in% bin){try(t <- dplyr::bind_rows(t, twoway_chi(data=data, x, treat, cens=cens, force.two=T, show.na=show.na)))}
     if (x %in% bin &         !x %in% dichotomize){try(t <- dplyr::bind_rows(t, twoway_chi(data=data, x, treat, cens=cens, bin=T, show.na=show.na)))}
     if (x %in% bin &          x %in% dichotomize){try(t <- dplyr::bind_rows(t, twoway_chi(data=data, x, treat, cens=cens, force.two=T, bin=T, show.na=show.na)))}
+    
+    # add count of missings
+    if (na.count==T){
+      groups <- unique(na.omit(data[[treat]]))
+      groups <- groups[order(groups)]
+      nas <- as.character()
+      for (g in groups){
+        nas <- c(nas, sum(is.na(data[[x]][data[[treat]]==g & !is.na(data[[treat]])])))
+      } 
+      t$nas[nrow(t)] <- paste0(nas, collapse="/")  
+    } 
   }
+  
+  # formatting
+  if (na.count==T){
+    t <- cbind(t[,colnames(t)!="nas"], t$nas)
+    colnames(t)[colnames(t) %in% c("nas", "t$nas")] <- paste0("NA's for ", paste0(groups, collapse="/")) 
+  } 
   t <- t %>% dplyr::mutate_if(.predicate = is.ok.num, function(x) as.num(as.chr(x)))
   t <- t %>% dplyr::mutate_if(.predicate = is.factor, function(x)       (as.chr(x)))
   return(t)
