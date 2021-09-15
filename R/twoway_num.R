728242cb47f55c268264ef26d37dced0f3dbad9b
socioskop <anders@socioskop.dk>
2021-09-16 00:10:00 +0200

    merging features from old ttools

--- R/twoway_num.R
+++ R/twoway_num.R
@@ -33,15 +33,15 @@ twoway_num <- function(data, x, group, weight, digit.m=1, digit.sd=1, cal.date=F
       tab[k+1] <- form.it(matrixStats::weightedSd    (data[[x]][data[[group]]==groups[i]], w=data$weight[data[[group]]==groups[i]], na.rm=T), digit.sd)
       tab[k+2] <- form.it(matrixStats::weightedMedian(data[[x]][data[[group]]==groups[i]], w=data$weight[data[[group]]==groups[i]], na.rm=T), digit.sd)
     } else {
-      tab[k+0] <- as.chr(lubridate::as_date(round(matrixStats::weightedMean(data[[x]][data[[group]]==groups[i]], w=data$weight[data[[group]]==groups[i]], na.rm=T))))
+      tab[k+0] <- as.chr(lubridate::as_date(round(matrixStats::weightedMean  (as.num(data[[x]][data[[group]]==groups[i]]), w=data$weight[data[[group]]==groups[i]], na.rm=T))))
       tab[k+1] <- form.it(matrixStats::weightedSd    (data[[x]][data[[group]]==groups[i]], w=data$weight[data[[group]]==groups[i]], na.rm=T), digit.sd)
-      tab[k+2] <- form.it(lubridate::as_date(round(matrixStats::weightedMedian(data[[x]][data[[group]]==groups[i]], w=data$weight[data[[group]]==groups[i]], na.rm=T))))
+      tab[k+2] <- as.chr(lubridate::as_date(round(matrixStats::weightedMedian(as.num(data[[x]][data[[group]]==groups[i]]), w=data$weight[data[[group]]==groups[i]], na.rm=T))))
     }
     
     # move on for next group
     k <- k+3
   }
-  print(tab)
+
   # decide which test to use
   if (test=="auto"){
     if(shapiro.test(resid(glm(paste0("as.num(", x, ")~factor(", group, ")"), data=data)))$p.value<shapiro.p){
